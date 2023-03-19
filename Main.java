2d0cbbd8bc0f5640cb2fe776b88cee4d2a6afe90
Cancerous115 <107626947+Cancerous115@users.noreply.github.com>
2023-03-19 16:56:01 +0000

    Comments added.

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main{
 	public static void main(String[]args){
-		//we can nest for loops aswell
+		//we can nest for loops aswell.if we cwant to add 10 we can just make "<" into "<=" for both for loops.
 		for(int number=1;number<10;number++){
 			for(int multiplier=1;multiplier<10;multiplier++){
 				System.out.printf("%dX%d=%d\n",number,multiplier,number*multiplier);
